e4a1367205a4d1bd25fdbb0aaf5e2a2f0d60685d
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2025-09-22 03:00:27 +0500

    adding another componet of star rating

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+// import App from "./App";
+// import "./index.css";
+import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating maxRating={5} />
+    <StarRating maxRating={10} />
   </React.StrictMode>
 );
