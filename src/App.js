8263ddd9d69a3f4558d7bc5d3e9cf7390d9cbb15
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2025-09-29 14:46:52 +0500

    implemented the abort controller function

--- src/App.js
+++ src/App.js
@@ -91,18 +91,22 @@ export default function App() {
 
   useEffect(
     function () {
+      // the use of the AbortController function to cancelled the unnecceasry requests while typing the query
+      const controller = new AbortController();
       async function fetchMovies() {
         try {
           setIsLoading(true); //setting the loader message before fetching the data
           setError(""); //before fetching the data always reset the error
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
           );
 
           if (!res.ok) throw new Error("SOMETHING WRONG WITH FETCHING MOVIES ");
           const data = await res.json();
           if (data.Response === "False") throw new Error("MOVIE NOT FOUND");
           setMovies(data.Search);
+          setError("");
           // console.log(data.Search);
           // setIsLoading(false); //set the loader to false after the movie data is fetched set the loading text to false
           // console.log(movies); this represent stale data as it will give output as an empty array, it will not immediately render the movies state instead the upper function of SetMovies needs tobe executed first
@@ -110,6 +114,9 @@ export default function App() {
         } catch (err) {
           console.error(err.message);
           setError(err.message);
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
@@ -119,6 +126,9 @@ export default function App() {
         }
       }
       fetchMovies();
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   ); // the empty array at the end is called as dependecny array
