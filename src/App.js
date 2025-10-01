42bfa8b7720cf1ab7a2de90d1d215828916c2588
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2025-10-01 18:08:30 +0500

    got to know about the lazy initialiser in the use state

--- src/App.js
+++ src/App.js
@@ -9,10 +9,15 @@ const KEY = "74bebcda";
 export default function App() {
   const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [selectedId, setSelectedId] = useState(null);
+  // const [watched, setWatched] = useState([]);
+  // learned about the call back initialisaton in sueState or this is called lazy initialiser
+  const [watched, setWatched] = useState(function () {
+    const storedValue = localStorage.getItem("watched");
+    return JSON.parse(storedValue);
+  });
 
   // useEffect(function () {
   //   console.log("after initial render or mount");
@@ -42,6 +47,13 @@ export default function App() {
   function handleDeleteWatched(id) {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
   }
+
+  useEffect(
+    function () {
+      localStorage.setItem("watched", JSON.stringify(watched));
+    },
+    [watched]
+  );
 
   useEffect(
     function () {
